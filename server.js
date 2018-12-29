69cbe97303d99f6098599eda45bb026e5f67d641
abhilive <cs.abhilive@gmail.com>
2018-12-29 18:26:04 +0530

    pdfjs: concurrent calls

--- server.js
+++ server.js
@@ -4,16 +4,16 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use('/',express.static('web/myweb/'));
- /* app.get('/', function (req,res) {
+app.use('/', express.static('web/myweb/'));
+/* app.get('/', function (req,res) {
      res.sendFile(path.join(__dirname + '/web/myweb/index.html'));
  }); */
 
- app.listen(PORT, () => {
-    console.log(`App is listening on port: ${PORT}`);
- });
+app.listen(PORT, () => {
+	console.log(`App is listening on port: ${PORT}`);
+});
 
- /* Node Way to handle request */
+/* Node Way to handle request */
 const fs = require('fs');
 const http = require('http');
 const url = require('url');
@@ -21,40 +21,67 @@ const url = require('url');
 var docDefinition = { content: 'This is an sample PDF printed with pdfMake' };
 
 const fileServer = http.createServer((req, res) => {
-    // Set CORS header
-    res.setHeader('Access-Control-Allow-Origin', '*');
+	// Set CORS header
+	res.setHeader('Access-Control-Allow-Origin', '*');
 	res.setHeader('Access-Control-Request-Method', '*');
 	res.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET');
-	res.setHeader('Access-Control-Allow-Headers', '*');
-    var path = url.parse(req.url).pathname;
-    switch(path) {
-        case '/pdf1':
-            try {
-                setTimeout(()=> {
-                    var file = fs.createReadStream('./web/compressed.tracemonkey-pldi-09.pdf');
-                    res.writeHead(200, {  
-                        'Content-Type': 'application/pdf' 
-                    });
-                    file.pipe(res);
-                }, 5*1000);
-                /* const data = fs.readFileSync('./web/pdfs/helloworld.pdf', 'utf-8');   
+	res.setHeader('Access-Control-Allow-Headers', 'Range');
+	res.setHeader(
+		'Access-Control-Expose-Headers',
+		'Accept-Ranges, Content-Encoding, Content-Length, Content-Range'
+	);
+	var path = url.parse(req.url).pathname;
+	switch (path) {
+		case '/pdf1':
+			/* const data = fs.readFileSync('./web/pdfs/helloworld.pdf', 'utf-8');   
                 res.end(data); */
-            } catch(err) {
-                if(err.code === 'ENOENT') {
-                    console.log('file not found.')
-                } else {
-                    throw err;
-                } 
-            }
-        break;
-        default:
-            res.writeHead(404);  
-            res.write("opps this doesn't exist - 404");  
-            res.end(); 
-        break;
-    }
+			/*try {
+				setTimeout(() => {
+					var file = fs.createReadStream(
+						'./web/pdfs/compressed.tracemonkey-pldi-09.pdf'
+					);
+					res.writeHead(200, {
+						'Content-Type': 'application/pdf',
+					});
+					file.pipe(res);
+				}, 5 * 1000);
+			} catch (err) {
+				if (err.code === 'ENOENT') {
+					console.log('file not found.');
+				} else {
+					throw err;
+				}
+      }*/
+			var file = fs.createReadStream(
+				'./web/pdfs/compressed.tracemonkey-pldi-09.pdf'
+			);
+			res.writeHead(200, {
+				'Content-Type': 'application/pdf',
+			});
+			file.pipe(res);
+			break;
+		case '/pdf2':
+			var file = fs.createReadStream('./web/pdfs/pdf2.pdf');
+			res.writeHead(200, {
+				'Content-Type': 'application/pdf',
+			});
+			file.pipe(res);
+			break;
+		case '/pdf3':
+			var file = fs.createReadStream('./web/pdfs/pdf3.pdf');
+			res.writeHead(200, {
+				'Content-Type': 'application/pdf',
+			});
+			file.pipe(res);
+			break;
+		default:
+			res.writeHead(404);
+			res.write("opps this doesn't exist - 404");
+			res.end();
+			break;
+	}
 });
 
 fileServer.listen(4242, () => {
-    console.log(`File server is running on port ${fileServer.address().port}.`);
+	console.log(`File server is running on port ${fileServer.address().port}.`);
 });
--- web/myweb/script.js
+++ web/myweb/script.js
@@ -1,60 +1,127 @@
+//const pdfjs = require('pdfjs');
+//console.log(pdfjs.version);
+
 // If absolute URL from the remote server is provided, configure the CORS
 // header on that server.
 //var url = '//cdn.mozilla.net/pdfjs/helloworld.pdf';
-var url = 'http://localhost:4242/pdf1';
 
 // Loaded via <script> tag, create shortcut to access PDF.js exports.
 var pdfjsLib = window['pdfjs-dist/build/pdf'];
 
 // The workerSrc property shall be specified.
 //pdfjsLib.GlobalWorkerOptions.workerSrc = '//mozilla.github.io/pdf.js/build/pdf.worker.js';
-pdfjsLib.GlobalWorkerOptions.workerSrc = './pdf.worker.js';
+//pdfjsLib.GlobalWorkerOptions.workerSrc = 'pdfjs-dist/build/pdf.worker.js';
 
 //pdfjsLib.workerPort = (pdfjsLib.GlobalWorkerOptions.workerPort === undefined ? null : pdfjsLib.GlobalWorkerOptions.workerPort);
-// Asynchronous download of PDF
-var params = {
-    url: url
-};
 
-var loadingTask = pdfjsLib.getDocument(params);
-loadingTask.promise.then(function(pdf) {
-  console.log('PDF loaded');
-  
-  // Fetch the first page
-  var pageNumber = 1;
-  pdf.getPage(pageNumber).then(function(page) {
-    console.log('Page loaded');
-    
-    var scale = 1.5;
-    var viewport = page.getViewport(scale);
+document.addEventListener('DOMContentLoaded', function(event) {
+	/**
+	 * Asynchronously downloads PDF.
+	 */
+	(function() {
+		var pdfDoc = null,
+			pageNum = 1,
+			pageRendering = false,
+			pageNumPending = null,
+			scale = 0.8;
 
-    // Prepare canvas using PDF page dimensions
-    var canvas = document.getElementById('the-canvas');
-    var context = canvas.getContext('2d');
-    canvas.height = viewport.height;
-    canvas.width = viewport.width;
+		var pdfUrls = {
+			url1: 'http://localhost:4242/pdf1',
+			url2: 'http://localhost:4242/pdf2',
+			url3: 'http://localhost:4242/pdf3',
+		};
 
-    // Render PDF page into canvas context
-    var renderContext = {
-      canvasContext: context,
-      viewport: viewport
-    };
-    var renderTask = page.render(renderContext);
-    renderTask.then(function () {
-      console.log('Page rendered');
-    });
-  });
-}, function (reason) {
-  // PDF loading error
-  console.error(reason);
-});
-document.addEventListener("DOMContentLoaded", function(event) {
-  document.getElementById("loadPDf").addEventListener("click", function(){
-    console.log('button clicked', this.getAttribute('action'));
+		function PdfJsObj(pdfUrl, pageNum, section) {
+			this.pdfUrl = pdfUrl;
+			this.pageNum = pageNum;
+			this.section = section;
+			this.pdfDoc = null;
+			this.callPDF = function() {
+				var _this = this;
+				pdfjsLib.getDocument(pdfUrl).promise.then(function(pdfDoc_) {
+					_this.pdfDoc = pdfDoc_;
+					_this.renderPage(_this.pageNum);
+				});
+			};
+			this.renderPage = function(pageNum) {
+				var _this = this;
+				this.pdfDoc.getPage(pageNum).then(function(page) {
+					//var viewport = page.getViewport(scale);
+					var section = _this.section;
+
+					// Prepare canvas using PDF page dimensions
+					var canvas = document.getElementById(
+						'the-canvas-' + section
+					);
+					var viewport = page.getViewport(
+						document.getElementsByClassName('pdf-container')[0]
+							.offsetWidth / page.getViewport(scale).width
+					);
+					var context = canvas.getContext('2d');
+					canvas.height = viewport.height;
+					canvas.width = viewport.width;
+					// Render PDF page into canvas context
+					var renderContext = {
+						canvasContext: context,
+						viewport: viewport,
+					};
+					var renderTask = page.render(renderContext);
+					console.log('pdfDoc.numPages: ', _this.pdfDoc.numPages);
+					// Wait for rendering to finish
+					renderTask.promise.then(function() {
+						if (pageNum < _this.pdfDoc.numPages) {
+							//renderPage(pageNumPending);
+						}
+					});
+				});
+			};
+		}
+
+		function createPDF(pdfUrl, pageNum, section) {
+			PdfJsObj.call(this, pdfUrl, pageNum, section);
+			console.log('Logging from function call #' + pdfUrl);
+		}
+
+		createPDF.prototype = Object.create(PdfJsObj.prototype);
+		createPDF.prototype.constructor = createPDF;
+
+		var pdf1 = new createPDF(pdfUrls.url1, pageNum, 'first');
+		var pdf2 = new createPDF(pdfUrls.url2, pageNum, 'second');
+		var pdf3 = new createPDF(pdfUrls.url3, pageNum, 'third');
+
+		setTimeout(pdf1.callPDF(), 0);
+		setTimeout(pdf2.callPDF(), 0);
+		setTimeout(pdf3.callPDF(), 0);
+	})();
+
+	/*function delayedLog(index, delay) {
+		setTimeout(function() {
+			console.log('Logging from function call #' + index);
+		}, delay);
+	}
+
+	delayedLog(1, 2000);
+	delayedLog(2, 1000);*/
+
+	/*pdfjsLib.getDocument(pdfUrls.url1).then(function(pdfDoc_) {
+		pdfDoc = pdfDoc_;
+		renderPage(pageNum, 'first');
+	});
+	pdfjsLib.getDocument(pdfUrls.url2).then(function(pdfDoc_) {
+		pdfDoc = pdfDoc_;
+		renderPage(pageNum, 'second');
+	});
+	pdfjsLib.getDocument(pdfUrls.url3).then(function(pdfDoc_) {
+		pdfDoc = pdfDoc_;
+		renderPage(pageNum, 'third');
+	});*/
+
+	/*document.getElementById("loadPDf").addEventListener("click", function() {
+    console.log("button clicked", this.getAttribute("action"));
   });
   var newCanvas = document.createElement("canvas");
   newCanvas.setAttribute("id", "the-canvas");
   newCanvas.setAttribute("class", "blockItem");
-  var bottomDiv = document.getElementById("bottom"); 
-  document.body.insertBefore(newCanvas, bottomDiv); 
+  var bottomDiv = document.getElementById("bottom");
+  document.body.insertBefore(newCanvas, bottomDiv);*/
 });
